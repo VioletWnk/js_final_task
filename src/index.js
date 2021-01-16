9d587bbc614f426c4e19afb5064fd69c972497e1
Violetta Lomako <violetta.lomako@gmail.com>
2021-01-16 22:26:35 +0300

    Update tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 import toggleModal from './modules/toggleModal';
+import scrollNav from './modules/scrollNav';
+import scrolltoTop from './modules/scrolltoTop';
+import faq from './modules/faq';
 
 toggleModal();
+scrollNav();
+scrolltoTop();
+faq();
--- src/modules/toggleModal.js
+++ src/modules/toggleModal.js
@@ -1,15 +1,22 @@
 const toggleModal = () => {
     const btnCallback = document.querySelectorAll('.callback-btn'),
+        btnServices = document.querySelector('.button-services'),
         modalCallback = document.getElementById('callback'),
         modalOverlay = document.querySelector('.modal-overlay'),
         modalClose = document.querySelector('.modal-close');
+
 
         btnCallback.forEach((btn) => {
             btn.addEventListener('click', () => {
                 modalCallback.style.display = 'flex';
                 modalOverlay.style.display = 'block';
           
-            })
+            });
+        });
+
+        btnServices.addEventListener('click', () => {
+            modalCallback.style.display = 'flex';
+            modalOverlay.style.display = 'block';
         });
 
         modalClose.addEventListener('click', () => {
