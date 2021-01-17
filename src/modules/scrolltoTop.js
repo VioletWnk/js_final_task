c75d0d672b61ad2a196b94bf03e2187ab6815c39
Violetta Lomako <violetta.lomako@gmail.com>
2021-01-17 09:06:30 +0300

    Update tasks

--- src/modules/scrolltoTop.js
+++ src/modules/scrolltoTop.js
@@ -1,15 +1,27 @@
 const scrolltoTop = () => {
-   const up = document.querySelector('.up');
+    const up = document.querySelector('.up'),
+        blockServices = document.getElementById('services');
 
-   up.addEventListener('click', () => {
-    
-    const scrollTarget = document.querySelector('body');
-    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    up.addEventListener('click', () => {
+        const scrollTarget = document.querySelector('body');
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+
         window.scrollBy({
             top: elementPosition,
             behavior: 'smooth'
         });
+    });
 
-   });
+    document.addEventListener('scroll', () => {
+
+        let yes = blockServices.getBoundingClientRect().top;
+ 
+            if(yes < 16){
+                up.style.display = 'block';
+            } else {
+                up.style.display = 'none';
+            }
+    });
+
 };
 export default scrolltoTop;
