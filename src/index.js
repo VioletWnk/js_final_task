e80a2c59497b64fe709fbaebbc1852cfeb35d72e
Violetta Lomako <violetta.lomako@gmail.com>
2021-01-17 17:40:55 +0300

    Update tasks

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import scrollNav from './modules/scrollNav';
 import scrolltoTop from './modules/scrolltoTop';
 import faq from './modules/faq';
 import sendFormData from './modules/sendFormData';
+import mainSlider from './modules/mainSlider';
 
 toggleModal();
 scrollNav();
 scrolltoTop();
 faq();
 sendFormData();
+mainSlider();
--- src/modules/sendFormData.js
+++ src/modules/sendFormData.js
@@ -1,7 +1,8 @@
 const sendFormData = () => {
     const errorMessage = 'Произошла ошибка',
         loadingMessage = 'Идет отправка...',
-        successMessage = 'Заявка успешно отправлена';
+        successMessage = 'Заявка успешно отправлена',
+        errorData = 'Введите корректные данные';
 
 
     const form = document.querySelector('[name="form-callback"]');
@@ -10,26 +11,48 @@ const sendFormData = () => {
     form.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        form.appendChild(message);
-        message.textContent = loadingMessage;
-        const data = new FormData(form);
-        postData(data)
-        .then((response) => {
-            if(response.status !== 200){
-                throw new Error('status network is not 200'); 
-            }
-            message.textContent = successMessage;
-            form.querySelectorAll('input').forEach((input) => {
-                if(input.type != 'submit'){
-                    input.value = '';
+        let validInputs = false;
+        form.querySelectorAll('input').forEach((input)  => {
+
+            if(input.matches('[name="tel"]')){
+                if(!input.value.match(/^\+?([78])?\d{6,10}$/)){
+                    return;
                 }
-                
-            }); 
-        })
-        .catch((error) => {
-            message.textContent = errorMessage;
-            console.error(error);
+            }
+            if(input.matches('[name="fio"]')){
+                if(!input.value.match(/^[а-яА-Я\s]{2,}$/ug)){
+                    return;
+                }
+            }
+            validInputs = true;
         });
+
+        if(validInputs){
+            form.appendChild(message);
+            message.textContent = loadingMessage;
+            const data = new FormData(form);
+            postData(data)
+            .then((response) => {
+                if(response.status !== 200){
+                    throw new Error('status network is not 200'); 
+                }
+                message.textContent = successMessage;
+                form.querySelectorAll('input').forEach((input) => {
+                    if(input.type != 'submit'){
+                        input.value = '';
+                    }
+                    
+                }); 
+            })
+            .catch((error) => {
+                message.textContent = errorMessage;
+                console.error(error);
+            });
+        } else {
+            message.textContent = errorData;
+        }
+
+        
     });
 
     const postData = (userData) => {
